dceb6acb1be30af59aae8156393f27d4a755befc
Jatin Shrivastava <71788769+jatinggg@users.noreply.github.com>
2025-11-27 22:51:17 +0530

    Add deployment confirmation print statement
    Added print statement for deployment confirmation.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bson.objectid import ObjectId
 from dotenv import load_dotenv
 import os
 
-# Load env vars
+# Load env var
 load_dotenv()
 
 app = Flask(__name__)
@@ -58,3 +58,5 @@ def delete_student(student_id):
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+
+print("deployed now")
